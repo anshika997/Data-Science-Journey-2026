9e434ab756daccfa4cd31044a1db9f0d73c46707
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-01-08 23:42:22 +0530

    Day 08 : revising again

---
+++ Day 8/variables_datatype_typecasting.py
@@ -0,0 +1,16 @@
+# VARIABLES 
+
+student=True
+Teacher=False
+a = 34
+b = 56.9
+name = "anshika"
+age = "19"
+print(type(int(age)))#typecasted
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(student))
+
+print(age) 
+print(type(age))
