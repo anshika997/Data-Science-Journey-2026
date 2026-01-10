e460e60a97745db72d480a50e419d52aee3c0735
Anshika Singh <anshikasinghbisen2006@gmail.com>
2026-01-10 22:21:32 +0530

    Day 10 : operators in python

---
+++ Day 10/operators.py
@@ -0,0 +1,68 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+print("-" * 50)
+
+x = 10
+print(x)
+x += 5
+print(x)
+x -= 3
+print(x)
+x *= 2
+print(x)
+x /= 4
+print(x)
+x %= 3
+print(x)
+print("-" * 50)
+
+p = 10
+q = 20
+print(p == q)
+print(p != q)
+print(p > q)
+print(p < q)
+print(p >= q)
+print(p <= q)
+print("-" * 50)
+
+m = True
+n = False
+print(m and n)
+print(m or n)
+print(not m)
+print("-" * 50)
+
+x = 5
+y = 3
+print(x & y)
+print(x | y)
+print(x ^ y)
+print(~x)
+print(x << 1)
+print(x >> 1)
+print("-" * 50)
+
+a = [1, 2, 3, 4]
+b = [1, 2, 3, 4]
+c = a
+
+print(a is b)
+print(a is c)
+print(a is not b)
+print("-" * 50)
+
+print(3 in a)
+print(10 not in a)
+print("-" * 50)
+
+print("Python" + " Operators")
+print(3 * "Hi ")
